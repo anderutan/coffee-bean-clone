5e78c9f9effaa37d6f2045d1c2ea12fa77f0e24a
anderu <andrewtan1310@gmail.com>
2024-06-24 14:14:25 +0800

    add filter by category and price slider

---
+++ src/components/FilterCatergoryPrice.tsx
@@ -0,0 +1,148 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { IoCloseSharp } from 'react-icons/io5';
+import { Separator } from './ui/separator';
+import { coffeeData } from '@/api/data';
+import ReactSlider from 'react-slider';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { BsFillChatSquareFill } from 'react-icons/bs';
+
+const FilterCatergoryPrice = () => {
+  const uniqueCategories = [
+    ...new Set(coffeeData.map((coffee) => coffee.category)),
+  ];
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const selectedCategories = searchParams.getAll('category');
+  const minPrice = Math.min(...coffeeData.map((coffee) => coffee.price));
+  const maxPrice = Math.max(...coffeeData.map((coffee) => coffee.price));
+
+  const initialPriceRange = [
+    Number(searchParams.get('minPrice')) || minPrice,
+    Number(searchParams.get('maxPrice')) || maxPrice,
+  ];
+
+  const [priceRange, setPriceRange] = useState(initialPriceRange);
+
+  const handleCategory = (e) => {
+    const category = e.target.value;
+    const newSelectedCategories = [...selectedCategories];
+
+    if (e.target.checked) {
+      if (!newSelectedCategories.includes(category)) {
+        newSelectedCategories.push(category);
+      }
+    } else {
+      const index = newSelectedCategories.indexOf(category);
+      if (index > -1) {
+        newSelectedCategories.splice(index, 1);
+      }
+    }
+
+    setSearchParams((prevParams) => {
+      const params = new URLSearchParams(prevParams);
+      params.delete('category');
+      newSelectedCategories.forEach((cat) => params.append('category', cat));
+      params.set('minPrice', String(priceRange[0]));
+      params.set('maxPrice', String(priceRange[1]));
+      return params;
+    });
+  };
+
+  const handleSliderChange = (value) => {
+    setPriceRange(value);
+    setSearchParams((prevParams) => {
+      const params = new URLSearchParams(prevParams);
+      params.set('minPrice', String(value[0]));
+      params.set('maxPrice', String(value[1]));
+      selectedCategories.forEach((cat) => params.append('category', cat));
+      return params;
+    });
+  };
+
+  useEffect(() => {
+    setPriceRange(initialPriceRange);
+  }, [searchParams]);
+
+  return (
+    <Drawer>
+      <DrawerTrigger className='w-full py-3 bg-[#E4E4E4] border-b-2 border-r-2 border-slate-300'>
+        Filter
+      </DrawerTrigger>
+      <DrawerContent className='h-[90%]'>
+        <DrawerClose className='absolute right-3 top-3'>
+          <IoCloseSharp className='w-8 h-8' />
+        </DrawerClose>
+        <DrawerHeader>
+          <DrawerTitle className='text-3xl text-left'>FILTER</DrawerTitle>
+          <span className='relative pl-4 after:absolute after:h-[5px] after:w-16 after:-bottom-0 after:left-0 after:bg-[#512D6D]'></span>
+        </DrawerHeader>
+        <div className='px-4'>
+          <p className='text-xl font-medium mb-5'>CATEGORY</p>
+          {uniqueCategories.map((category) => (
+            <CheckBox
+              category={category}
+              key={category}
+              checked={selectedCategories.includes(category)}
+              onChange={handleCategory}
+            />
+          ))}
+
+          <Separator />
+          <p className='text-xl font-medium my-5'>Price</p>
+          <ReactSlider
+            className='w-full h-[25px]'
+            thumbClassName='h-5 w-5 bg-purple-500 p-5 cursor-grab flex justify-center items-center text-white'
+            trackClassName='h-[8px] bg-[#512D6D]'
+            min={minPrice}
+            max={maxPrice}
+            value={priceRange}
+            onChange={handleSliderChange}
+            renderThumb={(props, state) => (
+              <div {...props}>{state.valueNow}</div>
+            )}
+          />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default FilterCatergoryPrice;
+
+type CheckBox = {
+  category: string;
+  checked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const CheckBox = ({ category, checked, onChange }: CheckBox) => {
+  return (
+    <div className='flex items-center space-x-2 pt-2 pb-4'>
+      <input
+        id={category.replace(/\s+/g, '')}
+        type='checkbox'
+        value={category}
+        checked={checked}
+        className='w-5 h-5 text-[#512D6D] border focus:ring-[#512D6D] rounded focus:ring-2 checked:bg-[#512D6D] '
+        onChange={onChange}
+      />
+
+      <label
+        htmlFor={category.replace(/\s+/g, '')}
+        className={`font-light text-[#512D6D] leading-none capitalize pl-2 ${
+          checked ? 'font-bold' : ''
+        }`}
+      >
+        {category}
+      </label>
+    </div>
+  );
+};
--- src/pages/CoffeeStore.tsx
+++ src/pages/CoffeeStore.tsx
@@ -1,13 +1,21 @@
+import FilterCatergoryPrice from '@/components/FilterCatergoryPrice';
 import PageBreadcrumb from '@/components/PageBreadcrumb';
 import { useLocation } from 'react-router-dom';
 
 const CoffeeStore = () => {
   const location = useLocation();
   const pathArray = location.pathname.split('/').filter((segment) => segment);
-  console.log(pathArray);
+
   return (
     <main className='px-3'>
       <PageBreadcrumb pathArray={pathArray} />
+      <div className='flex flex-col items-center pt-5 pb-10'>
+        <h1 className='text-3xl font-bold'>COFFEE</h1>
+        <span className='relative pl-4 after:absolute after:h-[5px] after:w-16 after:-bottom-5 after:-left-5 after:bg-[#512D6D]'></span>
+      </div>
+
+      {/* Filter function with shadcn drawer */}
+      <FilterCatergoryPrice />
     </main>
   );
 };
